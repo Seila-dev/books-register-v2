75b251538f163dc8816aedbd59723abafa3722fa
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-06-29 02:39:50 -0300

    fix: fix change category name input

--- src/app/home/categories/page.tsx
+++ src/app/home/categories/page.tsx
@@ -940,7 +940,10 @@ export default function AllCategoriesPage() {
                   <input
                     type="text"
                     value={newName}
-                    onChange={(e) => setNewName(e.target.value)}
+                    onChange={(e) => { 
+                      setNewName(e.target.value)
+                      e.stopPropagation()
+                    }}
                     className="flex-1 bg-gray-900 border border-gray-600 rounded-lg px-3 py-2 text-white"
                     autoFocus
                   />
